1ca328321595cb7ada0dddb2cdc0540c23879ab9
srikumartdba <158798132+srikumartdba@users.noreply.github.com>
2024-03-05 20:55:50 -0800

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,19 +1,16 @@
 variable "region" {
   description = "AWS region"
   type        = string
-  default     = "us-east-1"
 }
 
 variable "ami" {
   description = "AMI ID"
   type        = string
-  default     = "ami-05fa00d4c63e32376" # Default AMI ID
 }
 
 variable "instance_type" {
   description = "EC2 instance type"
   type        = string
-  default     = "t2.micro"
 }
 
 provider "aws" {
@@ -23,7 +20,8 @@ provider "aws" {
 resource "aws_instance" "srini-first-instance" {
   ami           = var.ami
   instance_type = var.instance_type
+
   tags = {
-      Name = "TF-Instance"
+    Name = "TF-Instance"
   }
 }
